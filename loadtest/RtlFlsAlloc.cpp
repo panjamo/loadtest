fe179f7b6f5a56862244094b68ae430ee33257a1
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2021-11-12 19:02:31 +0100

    es reicht jetzt

--- loadtest/RtlFlsAlloc.cpp
+++ loadtest/RtlFlsAlloc.cpp
@@ -1,3 +1,255 @@
+// TEB
+//    +0x000 NtTib            : _NT_TIB
+//    +0x038 EnvironmentPointer : (null) 
+//    +0x040 ClientId         : _CLIENT_ID
+//    +0x050 ActiveRpcHandle  : (null) 
+//    +0x058 ThreadLocalStoragePointer : (null) 
+//    +0x060 ProcessEnvironmentBlock : 0x000000dd`4eabd000 _PEB
+//    +0x068 LastErrorValue   : 0
+//    +0x06c CountOfOwnedCriticalSections : 0
+//    +0x070 CsrClientThread  : (null) 
+//    +0x078 Win32ThreadInfo  : (null) 
+//    +0x080 User32Reserved   : [26] 0
+//    +0x0e8 UserReserved     : [5] 0
+//    +0x100 WOW32Reserved    : (null) 
+//    +0x108 CurrentLocale    : 0x409
+//    +0x10c FpSoftwareStatusRegister : 0
+//    +0x110 ReservedForDebuggerInstrumentation : [16] (null) 
+//    +0x190 SystemReserved1  : [30] (null) 
+//    +0x280 PlaceholderCompatibilityMode : 0 ''
+//    +0x281 PlaceholderHydrationAlwaysExplicit : 0 ''
+//    +0x282 PlaceholderReserved : [10]  ""
+//    +0x28c ProxiedProcessId : 0
+//    +0x290 _ActivationStack : _ACTIVATION_CONTEXT_STACK
+//    +0x2b8 WorkingOnBehalfTicket : [8]  ""
+//    +0x2c0 ExceptionCode    : 0n0
+//    +0x2c4 Padding0         : [4]  ""
+//    +0x2c8 ActivationContextStackPointer : 0x000000dd`4eac6290 _ACTIVATION_CONTEXT_STACK
+//    +0x2d0 InstrumentationCallbackSp : 0
+//    +0x2d8 InstrumentationCallbackPreviousPc : 0
+//    +0x2e0 InstrumentationCallbackPreviousSp : 0
+//    +0x2e8 TxFsContext      : 0xfffe
+//    +0x2ec InstrumentationCallbackDisabled : 0 ''
+//    +0x2ed UnalignedLoadStoreExceptions : 0 ''
+//    +0x2ee Padding1         : [2]  ""
+//    +0x2f0 GdiTebBatch      : _GDI_TEB_BATCH
+//    +0x7d8 RealClientId     : _CLIENT_ID
+//    +0x7e8 GdiCachedProcessHandle : (null) 
+//    +0x7f0 GdiClientPID     : 0
+//    +0x7f4 GdiClientTID     : 0
+//    +0x7f8 GdiThreadLocalInfo : (null) 
+//    +0x800 Win32ClientInfo  : [62] 0
+//    +0x9f0 glDispatchTable  : [233] (null) 
+//    +0x1138 glReserved1      : [29] 0
+//    +0x1220 glReserved2      : (null) 
+//    +0x1228 glSectionInfo    : (null) 
+//    +0x1230 glSection        : (null) 
+//    +0x1238 glTable          : (null) 
+//    +0x1240 glCurrentRC      : (null) 
+//    +0x1248 glContext        : (null) 
+//    +0x1250 LastStatusValue  : 0
+//    +0x1254 Padding2         : [4]  ""
+//    +0x1258 StaticUnicodeString : _UNICODE_STRING ""
+//    +0x1268 StaticUnicodeBuffer : [261]  ""
+//    +0x1472 Padding3         : [6]  ""
+//    +0x1478 DeallocationStack : 0x000000dd`4f000000 Void
+//    +0x1480 TlsSlots         : [64] (null) 
+//    +0x1680 TlsLinks         : _LIST_ENTRY [ 0x00000000`00000000 - 0x00000000`00000000 ]
+//    +0x1690 Vdm              : (null) 
+//    +0x1698 ReservedForNtRpc : (null) 
+//    +0x16a0 DbgSsReserved    : [2] (null) 
+//    +0x16b0 HardErrorMode    : 0
+//    +0x16b4 Padding4         : [4]  ""
+//    +0x16b8 Instrumentation  : [11] (null) 
+//    +0x1710 ActivityId       : _GUID {00000000-0000-0000-0000-000000000000}
+//    +0x1720 SubProcessTag    : (null) 
+//    +0x1728 PerflibData      : (null) 
+//    +0x1730 EtwTraceData     : (null) 
+//    +0x1738 WinSockData      : (null) 
+//    +0x1740 GdiBatchCount    : 0
+//    +0x1744 CurrentIdealProcessor : _PROCESSOR_NUMBER
+//    +0x1744 IdealProcessorValue : 0
+//    +0x1744 ReservedPad0     : 0 ''
+//    +0x1745 ReservedPad1     : 0 ''
+//    +0x1746 ReservedPad2     : 0 ''
+//    +0x1747 IdealProcessor   : 0 ''
+//    +0x1748 GuaranteedStackBytes : 0
+//    +0x174c Padding5         : [4]  ""
+//    +0x1750 ReservedForPerf  : (null) 
+//    +0x1758 ReservedForOle   : (null) 
+//    +0x1760 WaitingOnLoaderLock : 0
+//    +0x1764 Padding6         : [4]  ""
+//    +0x1768 SavedPriorityState : (null) 
+//    +0x1770 ReservedForCodeCoverage : 0
+//    +0x1778 ThreadPoolData   : (null) 
+//    +0x1780 TlsExpansionSlots : (null) 
+//    +0x1788 DeallocationBStore : (null) 
+//    +0x1790 BStoreLimit      : (null) 
+//    +0x1798 MuiGeneration    : 0
+//    +0x179c IsImpersonating  : 0
+//    +0x17a0 NlsCache         : (null) 
+//    +0x17a8 pShimData        : (null) 
+//    +0x17b0 HeapData         : 0
+//    +0x17b4 Padding7         : [4]  ""
+//    +0x17b8 CurrentTransactionHandle : (null) 
+//    +0x17c0 ActiveFrame      : (null) 
+//    +0x17c8 FlsData          : (null) 
+//    +0x17d0 PreferredLanguages : (null) 
+//    +0x17d8 UserPrefLanguages : (null) 
+//    +0x17e0 MergedPrefLanguages : (null) 
+//    +0x17e8 MuiImpersonation : 0
+//    +0x17ec CrossTebFlags    : 0
+//    +0x17ec SpareCrossTebBits : 0y0000000000000000 (0)
+//    +0x17ee SameTebFlags     : 8
+//    +0x17ee SafeThunkCall    : 0y0
+//    +0x17ee InDebugPrint     : 0y0
+//    +0x17ee HasFiberData     : 0y0
+//    +0x17ee SkipThreadAttach : 0y1
+//    +0x17ee WerInShipAssertCode : 0y0
+//    +0x17ee RanProcessInit   : 0y0
+//    +0x17ee ClonedThread     : 0y0
+//    +0x17ee SuppressDebugMsg : 0y0
+//    +0x17ee DisableUserStackWalk : 0y0
+//    +0x17ee RtlExceptionAttached : 0y0
+//    +0x17ee InitialThread    : 0y0
+//    +0x17ee SessionAware     : 0y0
+//    +0x17ee LoadOwner        : 0y0
+//    +0x17ee LoaderWorker     : 0y0
+//    +0x17ee SkipLoaderInit   : 0y0
+//    +0x17ee SpareSameTebBits : 0y0
+//    +0x17f0 TxnScopeEnterCallback : (null) 
+//    +0x17f8 TxnScopeExitCallback : (null) 
+//    +0x1800 TxnScopeContext  : (null) 
+//    +0x1808 LockCount        : 0
+//    +0x180c WowTebOffset     : 0n0
+//    +0x1810 ResourceRetValue : (null) 
+//    +0x1818 ReservedForWdf   : (null) 
+//    +0x1820 ReservedForCrt   : 0
+//    +0x1828 EffectiveContainerId : _GUID {00000000-0000-0000-0000-000000000000}
+
+
+// PEB
+//    +0x000 InheritedAddressSpace : 0 ''
+//    +0x001 ReadImageFileExecOptions : 0 ''
+//    +0x002 BeingDebugged    : 0x1 ''
+//    +0x003 BitField         : 0x4 ''
+//    +0x003 ImageUsesLargePages : 0y0
+//    +0x003 IsProtectedProcess : 0y0
+//    +0x003 IsImageDynamicallyRelocated : 0y1
+//    +0x003 SkipPatchingUser32Forwarders : 0y0
+//    +0x003 IsPackagedProcess : 0y0
+//    +0x003 IsAppContainer   : 0y0
+//    +0x003 IsProtectedProcessLight : 0y0
+//    +0x003 IsLongPathAwareProcess : 0y0
+//    +0x004 Padding0         : [4]  ""
+//    +0x008 Mutant           : 0xffffffff`ffffffff Void
+//    +0x010 ImageBaseAddress : 0x00007ff7`2a940000 Void
+//    +0x018 Ldr              : 0x00007ffe`470b53a0 _PEB_LDR_DATA
+//    +0x020 ProcessParameters : 0x00000243`6b062b80 _RTL_USER_PROCESS_PARAMETERS
+//    +0x028 SubSystemData    : (null) 
+//    +0x030 ProcessHeap      : 0x00000243`6b060000 Void
+//    +0x038 FastPebLock      : 0x00007ffe`470b4fa0 _RTL_CRITICAL_SECTION
+//    +0x040 AtlThunkSListPtr : (null) 
+//    +0x048 IFEOKey          : (null) 
+//    +0x050 CrossProcessFlags : 1
+//    +0x050 ProcessInJob     : 0y1
+//    +0x050 ProcessInitializing : 0y0
+//    +0x050 ProcessUsingVEH  : 0y0
+//    +0x050 ProcessUsingVCH  : 0y0
+//    +0x050 ProcessUsingFTH  : 0y0
+//    +0x050 ProcessPreviouslyThrottled : 0y0
+//    +0x050 ProcessCurrentlyThrottled : 0y0
+//    +0x050 ProcessImagesHotPatched : 0y0
+//    +0x050 ReservedBits0    : 0y000000000000000000000000 (0)
+//    +0x054 Padding1         : [4]  ""
+//    +0x058 KernelCallbackTable : 0x00007ffe`461a8000 Void
+//    +0x058 UserSharedInfoPtr : 0x00007ffe`461a8000 Void
+//    +0x060 SystemReserved   : 0
+//    +0x064 AtlThunkSListPtr32 : 0
+//    +0x068 ApiSetMap        : 0x00000243`6af90000 Void
+//    +0x070 TlsExpansionCounter : 0
+//    +0x074 Padding2         : [4]  ""
+//    +0x078 TlsBitmap        : 0x00007ffe`470b5300 Void
+//    +0x080 TlsBitmapBits    : [2] 0x11fff
+//    +0x088 ReadOnlySharedMemoryBase : 0x00007ff4`f6110000 Void
+//    +0x090 SharedData       : (null) 
+//    +0x098 ReadOnlyStaticServerData : 0x00007ff4`f6110750  -> (null) 
+//    +0x0a0 AnsiCodePageData : 0x00007ff5`f8250000 Void
+//    +0x0a8 OemCodePageData  : 0x00007ff5`f8260228 Void
+//    +0x0b0 UnicodeCaseTableData : 0x00007ff5`f8270650 Void
+//    +0x0b8 NumberOfProcessors : 2
+//    +0x0bc NtGlobalFlag     : 0x70
+//    +0x0c0 CriticalSectionTimeout : _LARGE_INTEGER 0xffffe86d`079b8000
+//    +0x0c8 HeapSegmentReserve : 0x100000
+//    +0x0d0 HeapSegmentCommit : 0x2000
+//    +0x0d8 HeapDeCommitTotalFreeThreshold : 0x10000
+//    +0x0e0 HeapDeCommitFreeBlockThreshold : 0x1000
+//    +0x0e8 NumberOfHeaps    : 3
+//    +0x0ec MaximumNumberOfHeaps : 0x10
+//    +0x0f0 ProcessHeaps     : 0x00007ffe`470b3b80  -> 0x00000243`6b060000 Void
+//    +0x0f8 GdiSharedHandleTable : 0x00000243`6b4f0000 Void
+//    +0x100 ProcessStarterHelper : (null) 
+//    +0x108 GdiDCAttributeList : 0x14
+//    +0x10c Padding3         : [4]  ""
+//    +0x110 LoaderLock       : 0x00007ffe`470af4f8 _RTL_CRITICAL_SECTION
+//    +0x118 OSMajorVersion   : 0xa
+//    +0x11c OSMinorVersion   : 0
+//    +0x120 OSBuildNumber    : 0x4563
+//    +0x122 OSCSDVersion     : 0
+//    +0x124 OSPlatformId     : 2
+//    +0x128 ImageSubsystem   : 3
+//    +0x12c ImageSubsystemMajorVersion : 6
+//    +0x130 ImageSubsystemMinorVersion : 0
+//    +0x134 Padding4         : [4]  ""
+//    +0x138 ActiveProcessAffinityMask : 3
+//    +0x140 GdiHandleBuffer  : [60] 0
+//    +0x230 PostProcessInitRoutine : (null) 
+//    +0x238 TlsExpansionBitmap : 0x00007ffe`470b52e0 Void
+//    +0x240 TlsExpansionBitmapBits : [32] 1
+//    +0x2c0 SessionId        : 2
+//    +0x2c4 Padding5         : [4]  ""
+//    +0x2c8 AppCompatFlags   : _ULARGE_INTEGER 0x0
+//    +0x2d0 AppCompatFlagsUser : _ULARGE_INTEGER 0x0
+//    +0x2d8 pShimData        : 0x00000243`6afd0000 Void
+//    +0x2e0 AppCompatInfo    : (null) 
+//    +0x2e8 CSDVersion       : _UNICODE_STRING ""
+//    +0x2f8 ActivationContextData : 0x00000243`6afc0000 _ACTIVATION_CONTEXT_DATA
+//    +0x300 ProcessAssemblyStorageMap : (null) 
+//    +0x308 SystemDefaultActivationContextData : 0x00000243`6afb0000 _ACTIVATION_CONTEXT_DATA
+//    +0x310 SystemAssemblyStorageMap : (null) 
+//    +0x318 MinimumStackCommit : 0
+//    +0x320 FlsCallback      : 0x00000243`6b065a30 _FLS_CALLBACK_INFO
+//    +0x328 FlsListHead      : _LIST_ENTRY [ 0x00000243`6b0655f0 - 0x00000243`6b0655f0 ]
+//    +0x338 FlsBitmap        : 0x00007ffe`470b5360 Void
+//    +0x340 FlsBitmapBits    : [4] 0x1fff   <-------------------------- Achtund Speicher im PEB
+//    +0x350 FlsHighIndex     : 0xc
+//    +0x358 WerRegistrationData : (null) 
+//    +0x360 WerShipAssertPtr : (null) 
+//    +0x368 pUnused          : (null) 
+//    +0x370 pImageHeaderHash : (null) 
+//    +0x378 TracingFlags     : 0
+//    +0x378 HeapTracingEnabled : 0y0
+//    +0x378 CritSecTracingEnabled : 0y0
+//    +0x378 LibLoaderTracingEnabled : 0y0
+//    +0x378 SpareTracingBits : 0y00000000000000000000000000000 (0)
+//    +0x37c Padding6         : [4]  ""
+//    +0x380 CsrServerReadOnlySharedMemoryBase : 0x00007df4`7cfd0000
+//    +0x388 TppWorkerpListLock : 0
+//    +0x390 TppWorkerpList   : _LIST_ENTRY [ 0x000000dd`4edff730 - 0x000000dd`4efffbb0 ]
+//    +0x3a0 WaitOnAddressHashTable : [128] (null) 
+//    +0x7a0 TelemetryCoverageHeader : (null) 
+//    +0x7a8 CloudFileFlags   : 0
+//    +0x7ac CloudFileDiagFlags : 0
+//    +0x7b0 PlaceholderCompatibilityMode : 0 ''
+//    +0x7b1 PlaceholderCompatibilityModeReserved : [7]  ""
+//    +0x7b8 LeapSecondData   : 0x00007ff5`f8240000 _LEAP_SECOND_DATA
+//    +0x7c0 LeapSecondFlags  : 0
+//    +0x7c0 SixtySecondEnabled : 0y0
+//    +0x7c0 Reserved         : 0y0000000000000000000000000000000 (0)
+//    +0x7c4 NtGlobalFlag2    : 0
+
+
+
 
 longlong ********
 RtlFlsAlloc(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
@@ -5,12 +257,12 @@ RtlFlsAlloc(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 
 
 {
   longlong _NT_TIB;
-  longlong lVar2;
+  longlong PEB;
   code *pcVar3;
   uint uVar4;
-  longlong ********pppppppplVar5;
+  longlong ********callback_memory_2k;
   longlong ********pppppppplVar6;
-  longlong ********memory1;
+  longlong ********flsdata_memory;
   longlong lVar8;
   longlong ********rtn;
   undefined *puVar10;
@@ -30,32 +282,32 @@ RtlFlsAlloc(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 
 
   
   uVar4 = 0;
-  memory1 = rtn;
-  if (*(longlong *)(_NT_TIB + FlsData) == 0) { //_NT_TIB.FlsData
-    memory1 =
+  flsdata_memory = rtn;
+  if (*(longlong *)(_NT_TIB + FlsData /*(Offset 0x17c8)*/) == 0) { //_NT_TIB.FlsData
+    flsdata_memory =
          (longlong ********)
          RtlAllocateHeap(*(longlong *********)(*(longlong *)(in_GS_OFFSET + 0x60) + 0x30),
-                         NtdllBaseTag + 0x2c0000U | 8,(longlong ********)0x410);
-    if (memory1 == (longlong ********)0x0) {
+                         NtdllBaseTag + 0x2c0000U | 8,(longlong ********)0x410); /* 1040 bytes */
+    if (flsdata_memory == (longlong ********)0x0) {
       return (longlong ********)0xc0000017;
     }
-    *(longlong *********)(_NT_TIB + FlsData) = memory1;
+    *(longlong *********)(_NT_TIB + FlsData) = flsdata_memory;
   }
 
-  lVar2 = *(longlong *)(_NT_TIB + 0x60);
-  pppppppplVar5 = rtn;
-  if (*(longlong *)(lVar2 + 800) == 0) {
-    pppppppplVar5 =
+  PEB = *(longlong *)(_NT_TIB + 0x60);
+  callback_memory_2k = rtn;
+  if (*(longlong *)(PEB.FlsCallback) == 0) {
+    callback_memory_2k =
          (longlong ********)
          RtlAllocateHeap(*(longlong *********)(*(longlong *)(in_GS_OFFSET + 0x60) + 0x30),
-                         NtdllBaseTag + 0x2c0000,(longlong ********)0x800);
-    if (pppppppplVar5 == (longlong ********)0x0) {
+                         NtdllBaseTag + 0x2c0000,(longlong ********)0x800); /* 2k bytes*/
+    if (callback_memory_2k == (longlong ********)0x0) {
       rtn = (longlong ********)0xc0000017;
       uVar12 = extraout_XMM0_Qa;
       goto LAB_1800b62b4;
     }
-    lVar8 = 0x80;
-    pppppppplVar6 = pppppppplVar5;
+    lVar8 = 0x80; /* Zero memeory of 2k */
+    pppppppplVar6 = callback_memory_2k;
     do {
       *pppppppplVar6 = (longlong *******)0x0;
       pppppppplVar6[1] = (longlong *******)0x0;
@@ -63,47 +315,64 @@ RtlFlsAlloc(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 
       lVar8 = lVar8 + -1;
     } while (lVar8 != 0);
   }
+
+
+
+
   RtlAcquireSRWLockExclusive((ulonglong *)&RtlpFlsLock);
-  if ((pppppppplVar5 != (longlong ********)0x0) && (*(longlong *)(lVar2 + 800) == 0)) {
-    *(longlong *********)(lVar2 + 800) = pppppppplVar5;
-    pppppppplVar5 = rtn;
+  if ((callback_memory_2k != (longlong ********)0x0) && (*(longlong *)(PEB.FlsCallback) == 0)) {
+    *(longlong *********)(PEB.FlsCallback) = callback_memory_2k;
+    callback_memory_2k = rtn;
   }
-  if (memory1 == (longlong ********)0x0) {
+  if (flsdata_memory == (longlong ********)0x0) {
 LAB_180035e93:
-    uVar4 = RtlFindClearBitsAndSet(*(uint **)(lVar2 + 0x338),1,1);
-    if (uVar4 == 0xffffffff) goto LAB_1800b628a;
-    *(undefined8 *)(*(longlong *)(lVar2 + 800) + (ulonglong)uVar4 * 0x10) = param_5;
+
+    //    +0x338 FlsBitmap        : 0x00007ffe`470b5360 Void
+
+    // 0:004> dt ntdll!_RTL_BITMAP
+    //    +0x000 SizeOfBitMap     : Uint4B
+    //    +0x008 Buffer           : Ptr64 Uint4B
+
+    // 0:004>  dp 0x00007ffe`470b5360  l 2
+    // 00007ffe`470b5360  00000000`00000080 000000dd`4eab
+
+    // 0:004> db 000000dd`4eabd340 L 0x80/8
+    // 000000dd`4eabd340  ff 1f 00 00 00 00 00 00-00 00 00 00 00 00 00 00
+
+    uVar4 = RtlFindClearBitsAndSet(*(uint **)(PEB + FlsBitmap /* offset 0x338 */),1,1);
+    if (uVar4 == 0xffffffff) goto LAB_1800b628a (bail out);
+    *(undefined8 *)(*(longlong *)(PEB.FlsCallback) + (ulonglong)uVar4 * 0x10) = param_5;
     *(undefined8 *)(*(longlong *)(_NT_TIB + FlsData) + 0x10 + (ulonglong)uVar4 * 8) = 0;
     puVar11 = auStack72;
-    if (*(uint *)(lVar2 + 0x350) <= uVar4 && uVar4 != *(uint *)(lVar2 + 0x350)) {
-      *(uint *)(lVar2 + 0x350) = uVar4;
+    if (*(uint *)(PEB + 0x350) <= uVar4 && uVar4 != *(uint *)(PEB + 0x350)) {
+      *(uint *)(PEB + 0x350) = uVar4;
       rtn = (longlong ********)0x0;
       puVar11 = auStack72;
     }
   }
   else {
-    pppppppplVar6 = *(longlong *********)(lVar2 + 0x330);
-    if (*pppppppplVar6 == (longlong *******)(lVar2 + 0x328)) {
-      *memory1 = (longlong *******)(lVar2 + 0x328);
-      memory1[1] = (longlong *******)pppppppplVar6;
-      *pppppppplVar6 = (longlong *******)memory1;
-      *(longlong *********)(lVar2 + 0x330) = memory1;
-      memory1 = rtn;
+    pppppppplVar6 = *(longlong *********)(PEB + 0x330);
+    if (*pppppppplVar6 == (longlong *******)(PEB + "+0x328 FlsListHead")) {
+      *flsdata_memory = (longlong *******)(PEB + "+0x328 FlsListHead");
+      flsdata_memory[1] = (longlong *******)pppppppplVar6;
+      *pppppppplVar6 = (longlong *******)flsdata_memory;
+      *(longlong *********)(PEB + 0x330) = flsdata_memory;
+      flsdata_memory = rtn;
       goto LAB_180035e93;
     }
     pcVar3 = (code *)swi(0x29);
     (*pcVar3)(3);
     puVar10 = auStack64;
-LAB_1800b628a:
+LAB_1800b628a (bail out):
     rtn = (longlong ********)0xc0000017;
     puVar11 = puVar10;
   }
   *(undefined8 *)(puVar11 + -8) = 0x180035eec;
   uVar12 = RtlReleaseSRWLockExclusive((ulonglong *)&RtlpFlsLock);
-  if (pppppppplVar5 != (longlong ********)0x0) {
+  if (callback_memory_2k != (longlong ********)0x0) {
     pppppppplVar6 = *(longlong *********)(*(longlong *)(in_GS_OFFSET + 0x60) + 0x30);
     *(undefined8 *)(puVar11 + -8) = 0x1800b62ab;
-    RtlFreeHeap(uVar12,param_2,param_3,param_4,pppppppplVar6,0,pppppppplVar5);
+    RtlFreeHeap(uVar12,param_2,param_3,param_4,pppppppplVar6,0,callback_memory_2k);
     uVar12 = extraout_XMM0_Qa_00;
   }
   if (-1 < (int)rtn) {
@@ -111,11 +380,11 @@ LAB_1800b628a:
     return rtn;
   }
 LAB_1800b62b4:
-  if (memory1 != (longlong ********)0x0) {
+  if (flsdata_memory != (longlong ********)0x0) {
     *(undefined8 *)(_NT_TIB + FlsData) = 0;
-    pppppppplVar5 = *(longlong *********)(*(longlong *)(in_GS_OFFSET + 0x60) + 0x30);
+    callback_memory_2k = *(longlong *********)(*(longlong *)(in_GS_OFFSET + 0x60) + 0x30);
     *(undefined8 *)(puVar11 + -8) = 0x1800b62db;
-    RtlFreeHeap(uVar12,param_2,param_3,param_4,pppppppplVar5,0,memory1);
+    RtlFreeHeap(uVar12,param_2,param_3,param_4,callback_memory_2k,0,flsdata_memory);
   }
   return rtn;
 }
